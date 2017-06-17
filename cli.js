a13e4b0ea5d88fb17e5d3e2e461050da42c02ebf
tobihrbr <tobihrbr@gmail.com>
2017-06-17 14:22:52 +0200

    add cli

---
+++ cli.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+'use strict';
+
+const meow = require('meow');
+const isIp = require('is-ip');
+const longitude = require('longitude');
+
+const cli = meow(`
+	Usage
+	  $ longitude [ip]
+
+	Examples
+	  $ longitude
+	  Get geoip of this device
+
+	  $ longitude 8.8.8.8
+	  Get geoip of any ip
+
+`);
+
+if (cli.input[0] !== undefined) {
+	if (!isIp(cli.input[0])) {
+		console.log('No valid IP-Address specified');
+		process.exit(1);
+	}
+}
+
+longitude(cli.input[0]).then(data => {
+	console.log(data);
+});
